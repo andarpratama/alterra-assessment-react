f7850387351262aa2d184e32e760346d53f00649
andarpratama <andar.webdev@gmail.com>
2022-06-02 03:43:24 +0700

    installation and setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <main>
+        <h2>Home</h2>
+      </main>
+    );
+  }
+}
+
+export default Home;
